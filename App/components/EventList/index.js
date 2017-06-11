cde1a6839903ae7911f782418dd953c92eb336b6
Sarah <sarahforseti@gmail.com>
2017-06-11 12:16:04 -0500

    Creating EventList component

---
+++ App/components/EventList/index.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+import {
+    FlatList,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class EventList extends Component {
+    _getAllEvents() {
+        return [
+            {key: 1, date: 'July 1', name: 'Chemotherapy', volunteer: 'Jack'},
+            {key: 2, date: 'July 3rd', name: 'Toxicity Check', volunteer: 'TBD'},
+            {key: 3, date: 'July 7th', name: 'Weekend', volunteer: 'Sarah'}
+        ]
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+            <FlatList
+               data={ this._getAllEvents() }
+               renderItem={({item}) => <Text style={styles.item}>{item.date}: {item.name} - {item.volunteer}</Text>}
+            />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  item: {
+    fontSize: 20,
+    textAlign: 'left',
+    margin: 13,
+  },
+});
--- index.android.js
+++ index.android.js
@@ -1,32 +1,20 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
+  FlatList,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+import EventList from './App/components/EventList/';
 
 export default class caretaker extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+        <View style={styles.container}>
+            <Text style={styles.title}>Upcoming Events</Text>
+            <EventList />
+        </View>
     );
   }
 }
@@ -34,19 +22,12 @@ export default class caretaker extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
+  title: {
+    fontSize: 24,
     textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    margin: 15,
   },
 });
 
