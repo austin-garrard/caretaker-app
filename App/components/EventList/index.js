d35a466a14d5d3c8744fa69b6587cc30b8faaf13
Sarah <sarahforseti@gmail.com>
2017-06-14 14:45:41 -0500

    Adding a description modal

--- App/components/EventList/index.js
+++ App/components/EventList/index.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
 import {
+    Button,
     FlatList,
+    Modal,
     StyleSheet,
     Text,
+    TouchableOpacity,
     View
 } from 'react-native';
 import EventGateway from '../../gateway/event.js'
+import EventDetailsModal from '../../modals/EventDetailsModal'
 
 export default class EventListContainer extends Component {
   constructor() {
@@ -25,15 +29,85 @@ export default class EventListContainer extends Component {
   }
 }
 
+class MyListItem extends React.PureComponent {
+    _onPress = () => {
+        this.props.onPressItem(this.props.item);
+    };
+
+    render() {
+        item = this.props.item
+        return (
+            <TouchableOpacity onPress={this._onPress}>
+                <View style={styles.button}>
+                   <Text>{item.date}: {item.name} - {item.volunteer}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
+}
+
 export class EventList extends Component {
+    state = {
+        visibleModal: false,
+        event: {key: 1, date: 'July 1', time: '13:00-14:00', location: '6818 Austin Center Blvd, Austin, TX 78731', name: 'Chemotherapy', volunteer: 'Jack', role: 'driver', description: ''},
+    };
+
+    _eventDetails = (event) => (
+        <View>
+        <EventDetailsModal event />
+        </View>
+    );
+
+    _renderButton = (text, onPress) => (
+        <TouchableOpacity onPress={onPress}>
+          <View style={styles.button}>
+            <Text>{text}</Text>
+          </View>
+        </TouchableOpacity>
+    );
+
+    _renderModalContent = () => (
+        <View style={styles.modalContent}>
+              <Text>{this.state.event.name}</Text>
+
+              <Text>{this.state.event.date} {this.state.event.time}</Text>
+              <Text>{this.state.event.location}</Text>
+
+              <Text>{this.state.event.role}:{this.state.event.volunteer}</Text>
+
+              <Text>{this.state.event.description}</Text>
+            {this._renderButton('Close', () => this.setState({ visibleModal: false }))}
+        </View>
+    );
+
+    _onPressItem = (eventItem) => {
+        this.setState({ visibleModal: true, event: eventItem});
+    };
+
+    _renderItem = ({item}) => (
+        <MyListItem
+          id={item.key}
+          item={item}
+          onPressItem={this._onPressItem}
+          title={item.title}
+        />
+    );
+
     render() {
         return (
             <View style={styles.container}>
-            <Text style={styles.title}>Upcoming Events</Text>
-            <FlatList
-               data={this.props.events}
-               renderItem={({item}) => <Text style={styles.item}>{item.date}: {item.name} - {item.volunteer}</Text>}
-            />
+                <Modal visible={this.state.visibleModal === true}
+                    animationType={"fade"}
+                    transparent={false}
+                    onRequestClose={() => {alert("Modal has been closed.")}}>
+                      {this._renderModalContent()}
+                </Modal>
+                <Text style={styles.title}>Upcoming Events</Text>
+                <FlatList
+                   data={this.props.events}
+                   extraData={this.state}
+                   renderItem={this._renderItem}
+                />
             </View>
         );
     }
@@ -54,4 +128,7 @@ const styles = StyleSheet.create({
     textAlign: 'left',
     margin: 13,
   },
+  modalContent: {
+    margin: 13,
+  }
 });
--- App/gateway/event.js
+++ App/gateway/event.js
@@ -7,11 +7,11 @@
 export default class EventGateway {
   getAll() {
     return [
-      {key: 1, date: 'July 1', name: 'Chemotherapy', volunteer: 'Jack', role: 'driver'},
-      {key: 2, date: 'July 3rd', name: 'Toxicity Check', volunteer: 'Sarah', role: 'driver'},
-      {key: 3, date: 'July 5th', name: 'Checkin', volunteer: 'TBD', role: 'active'},
-      {key: 4, date: 'July 7th', name: 'Weekend', volunteer: 'Sarah', role: 'active'},
-      {key: 5, date: 'July 9th', name: 'Chemotherapy', volunteer: 'TBD', role: 'driver'}
+      {key: 1, date: 'July 1', time: '13:00-14:00', location: '6818 Austin Center Blvd, Austin, TX 78731', name: 'Chemotherapy', volunteer: 'Jack', role: 'driver', description: ''},
+      {key: 2, date: 'July 3rd', time: '11:00-12:00', location: '6818 Austin Center Blvd, Austin, TX 78731', name: 'Toxicity Check', volunteer: 'Sarah', role: 'driver', description: 'Quick check'},
+      {key: 3, date: 'July 5th', time: '08:30-09:30', location: '1120 S Lamar Blvd, Austin, TX 78704', name: 'Checkin', volunteer: 'TBD', role: 'active friend', description: ''},
+      {key: 4, date: 'July 7th', time: 'All Day', location: '1100 Congress Ave, Austin, TX 78701', name: 'Weekend', volunteer: 'Sarah', role: 'active friend', description: 'Need to keep busy'},
+      {key: 5, date: 'July 9th', time: '10:15-12:15', location: '6818 Austin Center Blvd, Austin, TX 78731', name: 'Chemotherapy', volunteer: 'TBD', role: 'driver', description: ''}
     ]
   }
 }
