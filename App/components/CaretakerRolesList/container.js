93b9bdf56cbb2476237b11a2a304b7eb15b1e463
Sarah <sarahforseti@gmail.com>
2017-06-27 21:25:02 -0500

    Basic Caretaker Role Menu screen with description and name coming from gateway

---
+++ App/components/CaretakerRolesList/container.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import CaretakerRolesGateway from '../../data/CaretakerRolesGateway';
+
+export var ContainerFor = (CaretakerRoleList) => class extends Component {
+    constructor() {
+        super();
+        this.state = {
+            caretakerRoles: [],
+            visibleModal: false,
+            selectedRole: null,
+        };        
+    }
+
+    updateRoles = () => {
+        let caretakerRoles = CaretakerRolesGateway.getAll();
+        let newState = Object.assign({}, this.state);
+        newState.caretakerRoles = caretakerRoles;
+        this.setState(newState);
+    }
+
+    componentDidMount() {
+        this.updateRoles();
+    }
+
+    _onPressItem = (roleId) => {
+        let newState = this.state;
+        newState.visibleModal = true;
+        newState.selectedRole = newState.caretakerRoles.find((role) => role.id === roleId)
+        this.setState(newState);
+    };
+
+    _onCloseModal = () => {
+        let newState = this.state;
+        newState.visibleModal = false;
+        this.setState(newState);
+    }
+
+    render() {
+        return <CaretakerRoleList
+            caretakerRoles={this.state.caretakerRoles}
+            visibleModal={this.state.visibleModal}
+            selectedRole={this.state.selectedRole}
+            onPressItem={this._onPressItem}
+            onCloseModal={this._onCloseModal}
+        />;
+    }
+}
