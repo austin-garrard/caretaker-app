0fba6b8cc4062bf4908f8ae17024f507e296c6c1
auroratide <tf.auroratide@gmail.com>
2017-07-13 09:50:10 -0500

    Updated theme with better pink color

--- App/config/theme.js
+++ App/config/theme.js
@@ -1,14 +1,15 @@
 const PINK = '#FF7575';
-const DARK_PINK = '#E66969';
+const DARK_PINK = '#CC5D5D';
 
 const theme = {
   palette: {
     primaryColor: PINK,
+    accentColor: PINK,
     secondaryColor: DARK_PINK
   },
   toolbar: {
     container: {
-      height: 50,
+      height: 50
     },
   },
 };
