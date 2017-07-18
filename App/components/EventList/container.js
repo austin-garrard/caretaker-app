1503aa02be82361aacf54c17a65ac1ea557bf4c4
auroratide <tf.auroratide@gmail.com>
2017-07-18 11:19:29 -0500

    Applied new event list to the event list for Patients

--- App/components/EventList/container.js
+++ App/components/EventList/container.js
@@ -3,57 +3,38 @@ import EventGateway from '../../data/EventGateway';
 import UserGateway from '../../data/UserGateway';
 
 export var ContainerFor = (EventList) => class extends Component {
-    constructor() {
-        super();
-        this.state = {
-            events: [],
-            visibleModal: false,
-            selectedEvent: null,
-        };
-        EventGateway.subscribe(this.updateEvents);
-    }
-
-    updateEvents = () => {
-        let events = EventGateway.getAll();
-        events.forEach((event) => event.volunteer = UserGateway.getName(event.volunteerId));
-        let newState = Object.assign({}, this.state);
-        newState.events = events;
-        this.setState(newState);
-    }
-
-    componentDidMount() {
-        this.updateEvents();
-    }
-
-    componentWillUnmount() {
-        EventGateway.unsubscribe(this.updateEvents);
-    }
-
-    _onPressItem = (eventId) => {
-        let newState = this.state;
-        newState.visibleModal = true;
-        newState.selectedEvent = newState.events.find((event) => event.id === eventId)
-        this.setState(newState);
+  constructor() {
+    super();
+    this.state = {
+        events: []
     };
+    EventGateway.subscribe(this.updateEvents);
+  }
 
-    _onCloseModal = () => {
-        let newState = this.state;
-        newState.visibleModal = false;
-        this.setState(newState);
-    }
+  updateEvents = () => {
+    let events = EventGateway.getAll();
+    events.forEach((event) => event.volunteer = UserGateway.getName(event.volunteerId));
+    let newState = Object.assign({}, this.state);
+    newState.events = events;
+    this.setState(newState);
+  }
 
-    _onCreateEvent = (data) => {
-      EventGateway.createNewEvent(data);
-    }
+  componentDidMount() {
+    this.updateEvents();
+  }
 
-    render() {
-        return <EventList
-            events={this.state.events}
-            visibleModal={this.state.visibleModal}
-            selectedEvent={this.state.selectedEvent}
-            onPressItem={this._onPressItem}
-            onCloseModal={this._onCloseModal}
-            onCreateEvent={this._onCreateEvent}
-        />;
-    }
+  componentWillUnmount() {
+    EventGateway.unsubscribe(this.updateEvents);
+  }
+
+  _onCreateEvent = (data) => {
+    EventGateway.createNewEvent(data);
+  }
+
+  render() {
+    return <EventList
+      events={this.state.events}
+      onCreateEvent={this._onCreateEvent}
+    />;
+  }
 }
--- App/components/EventList/test.js
+++ App/components/EventList/test.js
@@ -48,41 +48,6 @@ describe('EventList', () => {
         events.forEach((event) => expect(spy).toHaveBeenCalledWith(event.volunteerId));
     });
 
-    describe('_onPressItem', () => {
-        it('should make the modal visible', () => {
-            const list = renderer
-                .create(<EventList />)
-                .getInstance();
-
-            list._onPressItem(1);
-
-            expect(list.state.visibleModal).toBe(true);
-        });
-
-        it('should set the selectedEvent', () => {
-            const events = EventGateway.getAll(); //todo: stub this out
-            const list = renderer
-                .create(<EventList />)
-                .getInstance();
-
-            list._onPressItem(1);
-
-            expect(list.state.selectedEvent).toBe(events[0]);
-        });
-    });
-
-    describe('_onCloseModal', () => {
-        it('should make the modal not visible', () => {
-            const list = renderer
-                .create(<EventList />)
-                .getInstance();
-
-            list._onCloseModal();
-
-            expect(list.state.visibleModal).toBe(false);
-        });
-    });
-
     describe('_onCreateEvent', () => {
       it('should create an event through the gateway', () => {
         const spy = jest.spyOn(EventGateway, 'createNewEvent');
